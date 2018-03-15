1eba79751c5c6cf8c39f533cf4ecd7e6284a5fac
kimyoungji <youngji.brigid.kim@gmail.com>
2018-03-15 17:52:44 +0900

    modified to subscribe and publish

--- monodepth_simple.py
+++ monodepth_simple.py
@@ -22,6 +22,14 @@ import tensorflow.contrib.slim as slim
 import scipy.misc
 import matplotlib.pyplot as plt
 
+import roslib
+import sys
+import rospy
+import cv2
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
 from monodepth_model import *
 from monodepth_dataloader import *
 from average_gradients import *
@@ -46,65 +54,85 @@ def post_process_disparity(disp):
     r_mask = np.fliplr(l_mask)
     return r_mask * l_disp + l_mask * r_disp + (1.0 - l_mask - r_mask) * m_disp
 
-def test_simple(params):
-    """Test function."""
+class monodepth:
+    def __init__(self):
 
-    left  = tf.placeholder(tf.float32, [2, args.input_height, args.input_width, 3])
-    model = MonodepthModel(params, "test", left, None)
+        '''Initialize ros publisher, ros subscriber'''
+        self.image_pub = rospy.Publisher("/monodepth/image",Image)
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber("/kitti/left_color_image",Image,self.callback)
 
-    input_image = scipy.misc.imread(args.image_path, mode="RGB")
-    original_height, original_width, num_channels = input_image.shape
-    input_image = scipy.misc.imresize(input_image, [args.input_height, args.input_width], interp='lanczos')
-    input_image = input_image.astype(np.float32) / 255
-    input_images = np.stack((input_image, np.fliplr(input_image)), 0)
+        '''Initialize network for the depth estimation'''
+        params = monodepth_parameters(
+            encoder=args.encoder,
+            height=args.input_height,
+            width=args.input_width,
+            batch_size=2,
+            num_threads=1,
+            num_epochs=1,
+            do_stereo=False,
+            wrap_mode="border",
+            use_deconv=False,
+            alpha_image_loss=0,
+            disp_gradient_loss_weight=0,
+            lr_loss_weight=0,
+            full_summary=False)
 
-    # SESSION
-    config = tf.ConfigProto(allow_soft_placement=True)
-    sess = tf.Session(config=config)
+        self.left  = tf.placeholder(tf.float32, [2, args.input_height, args.input_width, 3])
+        self.model = MonodepthModel(params, "test", self.left, None)
 
-    # SAVER
-    train_saver = tf.train.Saver()
+        # SESSION
+        config = tf.ConfigProto(allow_soft_placement=True)
+        self.sess = tf.Session(config=config)
 
-    # INIT
-    sess.run(tf.global_variables_initializer())
-    sess.run(tf.local_variables_initializer())
-    coordinator = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(sess=sess, coord=coordinator)
+        # SAVER
+        train_saver = tf.train.Saver()
 
-    # RESTORE
-    restore_path = args.checkpoint_path.split(".")[0]
-    train_saver.restore(sess, restore_path)
+        # INIT
+        self.sess.run(tf.global_variables_initializer())
+        self.sess.run(tf.local_variables_initializer())
+        coordinator = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=self.sess, coord=coordinator)
 
-    disp = sess.run(model.disp_left_est[0], feed_dict={left: input_images})
-    disp_pp = post_process_disparity(disp.squeeze()).astype(np.float32)
+        # RESTORE
+        restore_path = args.checkpoint_path.split(".")[0]
+        train_saver.restore(self.sess, restore_path)
 
-    output_directory = os.path.dirname(args.image_path)
-    output_name = os.path.splitext(os.path.basename(args.image_path))[0]
+    def callback(self,data):
+        input_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        cv2.imshow("Image window", input_image)
+        cv2.waitKey(3)
 
-    np.save(os.path.join(output_directory, "{}_disp.npy".format(output_name)), disp_pp)
-    disp_to_img = scipy.misc.imresize(disp_pp.squeeze(), [original_height, original_width])
-    plt.imsave(os.path.join(output_directory, "{}_disp.png".format(output_name)), disp_to_img, cmap='plasma')
+        original_height, original_width, num_channels = input_image.shape
+        input_image = scipy.misc.imresize(input_image, [args.input_height, args.input_width], interp='lanczos')
+        input_image = input_image.astype(np.float32) / 255
+        input_images = np.stack((input_image, np.fliplr(input_image)), 0)
 
-    print('done!')
+        self.test_simple(input_images,original_height,original_width)
+
+
+    def test_simple(self,input_images,original_height,original_width):
+        """Test function."""
+
+        disp = self.sess.run(self.model.disp_left_est[0], feed_dict={self.left: input_images})
+        disp_pp = post_process_disparity(disp.squeeze()).astype(np.float32)
+#        disp_pp = disp[0,:,:].astype(np.float32)
+
+        disp_to_img = scipy.misc.imresize(disp_pp.squeeze(), [original_height, original_width])
+        cv2.imshow("disp_to_img", disp_to_img)
+        cv2.waitKey(3)
+        
+        self.image_pub.publish(self.bridge.cv2_to_imgmsg(disp_to_img, "mono8"))
+
 
 def main(_):
 
-    params = monodepth_parameters(
-        encoder=args.encoder,
-        height=args.input_height,
-        width=args.input_width,
-        batch_size=2,
-        num_threads=1,
-        num_epochs=1,
-        do_stereo=False,
-        wrap_mode="border",
-        use_deconv=False,
-        alpha_image_loss=0,
-        disp_gradient_loss_weight=0,
-        lr_loss_weight=0,
-        full_summary=False)
+    ic = monodepth()
 
-    test_simple(params)
+    #init rospy
+    rospy.init_node('monodepth', anonymous=True)
+
+    rospy.spin()
 
 if __name__ == '__main__':
     tf.app.run()
